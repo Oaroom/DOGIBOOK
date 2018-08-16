0e20597ef960a9bc3edff64def74232240f14a6d
oaroom <unidevel2013@gmail.com>
2018-08-16 16:40:47 +0900

    게시판

--- dogibook/dogibook.js
+++ dogibook/dogibook.js
@@ -1,5 +1,19 @@
-<script type="text/javascript">
-$(".dropmenu ul li").hover(function(){
-  $(this).find("ul").stop().fadeToggle(300);
-});
-</script>
+
+
+function submit(){
+
+  alert('팁이 공유되었습니다.');
+  location.href="commu.1.html";
+
+
+}
+
+
+function plan(){
+
+  alert('약속이 등록되었습니다.');
+  location.href="commu.2.html";
+
+}
+
+
